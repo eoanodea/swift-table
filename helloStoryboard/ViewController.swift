d651aae823c7444a8ed3d9df9fae3a4822d3ab3a
Eoan O Dea <odeaeoan@gmail.com>
2020-02-13 19:01:20 +0000

    Created basic application

--- helloStoryboard/ViewController.swift
+++ helloStoryboard/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    
+    var num = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.textColor = #colorLiteral(red: 1, green: 0, blue: 1, alpha: 1)
+        myLabel.text = "Yeehaw"
+        myTextField.becomeFirstResponder()
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        num = num + 1
+        myLabel.text = "\(num) Yeehaws for \(myTextField.text!)"
+        myTextField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        myTextField.resignFirstResponder()
+    }
+    
 }
 
